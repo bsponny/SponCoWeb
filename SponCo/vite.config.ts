e3e2ef206e8bd9cc63bf05443407946822bf7dea
sponcoadmin <sponcoadmin@SPONENBURGH.local>
2023-06-06 22:22:03 -0600

    added web config creation

--- SponCo/vite.config.ts
+++ SponCo/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import WebConfig from 'vite-plugin-web-config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+	  react(),
+	  WebConfig({
+		  proxy: [['api', 'http://localhost:3000']],
+		  output: 'web.config',
+	  }),
+  ],
 })
